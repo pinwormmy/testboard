a4b1b7eb1271f061097b18652a97d0359bcf4dc1
pinwormmy <ozkhan@naver.com>
2021-09-27 01:47:48 +0900

    9.27
    커밋이 잘 안되
    다시 등록

---
+++ BoardStudy/src/main/java/com/board/domain/ReplyVO.java
@@ -0,0 +1,12 @@
+package com.board.domain;
+
+import java.util.Date;
+
+public class ReplyVO {
+
+	private int rno;
+	private int bno;
+	private String writer;
+	private String content;
+	private Date regDate;
+}
