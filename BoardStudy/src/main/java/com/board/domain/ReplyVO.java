177c16dc8e9d36f2ebc7abe994f30b7b0d4ed3f7
pinwormmy <ozkhan@naver.com>
2021-09-27 18:31:29 +0900

    9.27
    커밋이 자꾸 제대로 안올라가네 샹샹...

--- BoardStudy/src/main/java/com/board/domain/ReplyVO.java
+++ BoardStudy/src/main/java/com/board/domain/ReplyVO.java
@@ -3,6 +3,20 @@ package com.board.domain;
 import java.util.Date;
 
 public class ReplyVO {
+	
+	/*
+	 
+	CREATE TABLE tbl_reply(
+		rno		int		not null auto_increment,
+		bno		int 	not null,
+		writer	varchar(30) not null,
+		content	text	not null,
+		regDate	timestamp not null default now(),
+		PRIMARY KEY(rno, bno),
+		FOREIGN KEY(bno) REFERENCES tbl_board(bno)
+		);
+
+	 */
 
 	private int rno;
 	private int bno;
